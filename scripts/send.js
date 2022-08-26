a87076622bc7ff36daff86a4bda160b932f80883
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-08-26 00:01:48 -0300

    fix: updated message

--- scripts/send.js
+++ scripts/send.js
@@ -4,9 +4,8 @@ const { sendMessage } = require('../src/sqs.js');
 const QUEUE_URL = process.env.QUEUE_URL || 'http://localhost:4566/000000000000/BOOKS_QUEUE.fifo'; 
 
 const message = {
-	name: 'Jhon Doe',
-	age: 23,
-	date: new Date(),
+	title: 'Clean Architecture',
+	author: 'Robert Martin',
 }
 
 sendMessage(
