81403c39c0fc900abaaa242cea0111d9feccadf7
AlvaroDavi5 <alvaro-davi1@hotmail.com>
2022-09-07 01:35:23 -0300

    doc: updated shell script

--- SQS_sample.sh
+++ SQS_sample.sh
@@ -5,16 +5,17 @@
 awslocal --endpoint-url=http://localhost:4566 sqs create-queue --queue-name=test_queue.fifo --attributes FifoQueue=true --region=us-east-1 --output=table | cat
 
 # list SQS queue
-awslocal sqs list-queues
+awslocal sqs list-queues --output=table | cat
 
 # send message to SQS queue
-awslocal sqs send-message --message-body="{'message': 'Hello World', 'error': null}" --message-group-id="Group1" --message-deduplication-id="mock" --queue-url=http://localhost:4566/000000000000/test_queue.fifo
+awslocal sqs send-message --message-body="{'message': 'Hello World', 'error': null}" --message-group-id="Group1" --message-deduplication-id="mock" --queue-url=http://localhost:4566/000000000000/test_queue.fifo --output=table | cat
 
 # receive message from SQS queue
 awslocal --endpoint-url=http://localhost:4566 sqs receive-message --queue-url=http://localhost:4566/000000000000/test_queue.fifo --region=us-east-1 --output=json | cat
 
 # delete message from SQS queue
-awslocal sqs delete-message --endpoint-url=http://localhost:4566 --queue-url=http://localhost:4566/000000000000/test_queue.fifo --region=us-east-1  --receipt-handle=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+awslocal sqs delete-message --endpoint-url=http://localhost:4566 --queue-url=http://localhost:4566/000000000000/test_queue.fifo --region=us-east-1  --receipt-handle=XXXXXXXXXX --output=table | cat
 
 # delete SQS queue
-awslocal sqs delete-queue --queue-url=http://localhost:4566/000000000000/test_queue.fifo
+awslocal sqs delete-queue --queue-url=http://localhost:4566/000000000000/test_queue.fifo --output=table | cat
+
